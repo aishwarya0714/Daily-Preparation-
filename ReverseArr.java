850c69d5c94a050f166241a150726be82eed20e3
aishwarya0714 <136805991+aishwarya0714@users.noreply.github.com>
2023-06-19 21:16:39 +0530

    Add files via upload

---
+++ ReverseArr.java
@@ -0,0 +1,13 @@
+public class ReverseArr {
+    public static void main(String[] args){
+    int[] arr = {3,5,1,2};
+
+/* last element should print
+    arr.length-1 imdex
+
+*/
+    for(int i = arr.length -1 ; i >=0 ;i--){
+        System.out.println(arr[i]);
+    }
+}
+}
