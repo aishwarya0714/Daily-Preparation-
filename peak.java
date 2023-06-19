12a967d895a0d351914e82e1689ebb62ffc09b90
aishwarya0714 <136805991+aishwarya0714@users.noreply.github.com>
2023-06-19 21:17:10 +0530

    Update peak.java

--- peak.java
+++ peak.java
@@ -3,7 +3,7 @@ public class peak {
     public static void main(String[] args){
         int[] arr = {12,56,23,90,78};
 
-        // int peakElm;
+      
 
         for (int i = 1; i < arr.length -1 ; i++){
             if (arr[i] > arr[i-1] & arr[i] > arr[i+1]){
